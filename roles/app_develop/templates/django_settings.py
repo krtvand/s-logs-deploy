389f19c10bb62676133ba4fede8861a85d562cf0
Andrey Kartaev <a.kartaev@qrator.net>
2020-01-23 23:25:29 +0300

    working app

---
+++ roles/app_develop/templates/django_settings.py
@@ -0,0 +1,19 @@
+from sorm_logs.settings import *
+
+DATABASES = {
+    'default': {
+        'NAME': 'sorm_logs',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': '{{ mysql_user }}',
+        'PASSWORD': '{{ mysql_password }}',
+        'HOST': '{{ sorm_logs_mariadb_docker_container_name }}',
+        'PORT': "3306"
+    },
+    'clickhouse': {
+        'url': 'http://{{ sorm_logs_clickhouse_docker_container_name }}:8123/',
+    }
+}
+
+SORM_LOGS_BACKUP = {
+    'url': 'http://{{ sorm_logs_backup_docker_container_name }}:80/make_clickhouse_backup'
+}
