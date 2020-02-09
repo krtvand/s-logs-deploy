9d6f07135e650ffc300643796350b84dc6630cd6
krtvand <a.kartaev@qrator.net>
2020-02-09 23:53:06 +0300

    some

--- roles/app/templates/django_settings.py
+++ roles/app/templates/django_settings.py
@@ -6,10 +6,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'USER': '{{ mysql_user }}',
         'PASSWORD': '{{ mysql_password }}',
-        'HOST': 'sorm_logs_mariadb',
+        'HOST': '{{ sorm_logs_mariadb_docker_container_name }}',
         'PORT': "3306"
     },
     'clickhouse': {
-        'url': 'http://sorm_logs_clickhouse:8123/'
+        'url': 'http://{{ sorm_logs_clickhouse_docker_container_name }}:8123/',
     }
+}
+
+SORM_LOGS_BACKUP = {
+    'url': 'http://{{ sorm_logs_backup_docker_container_name }}:80/make_clickhouse_backup'
 }
